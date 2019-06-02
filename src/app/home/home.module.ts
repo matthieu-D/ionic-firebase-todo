e51a3c42e87ae127a94e68cc44fac88b446aa8bd
e <e>
2019-06-02 14:26:00 +0200

    update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,6 +4,13 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
+import {
+  AngularFireDatabase,
+  AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
 import { HomePage } from './home.page';
 
 @NgModule({
@@ -11,12 +18,17 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     RouterModule.forChild([
       {
         path: '',
         component: HomePage
       }
     ])
+  ],
+  providers: [
+    AngularFireDatabase
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tasksRef: AngularFireList<any>;
+  tasks: Observable<any[]>;
+  newTask = {name: ''};
 
-  constructor() {}
+  constructor(public db: AngularFireDatabase) {
+    this.tasksRef = db.list('/tasks');
 
+    this.tasks = this.tasksRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+
+  addTask(newTask) {
+    this.tasksRef.push(newTask);
+    this.newTask = {name: ''};
+  }
+
+  removeTask(taskKey) {
+    this.tasksRef.remove(taskKey);
+  }
+
+  updateTask(key, name) {
+    this.tasksRef.update(key, {name: name});
+  }
 }
